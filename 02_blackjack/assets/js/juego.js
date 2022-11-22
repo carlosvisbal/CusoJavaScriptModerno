07b5599582de84f7a28ff19746f48a85bb60aa5b
carlosvisbal <carlosvisbal66@gmail.com>
2022-11-21 21:27:17 -0500

    Curso JavaScript
    Curso JavaScript

--- 02_blackjack/assets/js/juego.js
+++ 02_blackjack/assets/js/juego.js
@@ -8,6 +8,7 @@
 let deck         = []
 const tipos      = ['C', 'D', 'H', 'S']
 const especiales = ['A', 'J', 'Q', 'K']
+// Esta funcion crea un nuevo deck
 const creaDeck =()=>{
     for (let i=2; i<=10; i++){
         for (let tipo of tipos){
@@ -26,9 +27,46 @@ const creaDeck =()=>{
 
     }
     console.log(deck);
+    // Con esta funcion se revuelve los elementos dentro de deck (shuffle)
     deck = _.shuffle(deck)
-    console.log(deck[1]);
+    console.log(deck);
 
 }
 
 creaDeck()
+
+pedirCarta = () => {
+    if (deck.length === 0){
+        throw 'No hay cartas en el deck'
+    }
+    let carta = deck.pop()
+    // console.log(carta);
+    return carta
+}
+
+// const valorCarta  = (carta) => {
+//     const valor = carta.substring(0, carta.length - 1)
+//     let puntos = 0
+//     console.log({valor});
+//     if(isNaN(valor)){
+//         console.log('No es un numero');
+//         puntos = ( valor === 'A' ) ? 11 : 10
+//         console.log({puntos});
+//     }else{
+//         puntos = valor * 1
+//         console.log({puntos});
+//         console.log('Es un numero');
+//     }
+//     return valor
+// }
+
+const valorCarta = (carta) => {
+    console.log({carta});
+    const valor = carta.substring(0, carta.length - 1)
+    return (isNaN(valor)) ? 
+            (valor === 'A' ) ? 11 : 10
+            : valor * 1 
+}
+
+const valor = valorCarta(pedirCarta())
+console.log({valor});
